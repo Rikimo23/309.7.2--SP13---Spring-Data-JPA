9d623987a63edc1bd633ff4b764025a6d0a858c3
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-08-26 15:02:02 -0400

    finished

---
+++ src/main/java/com/exampleCrudDemo/Spring/Initilizr/SpringInitilizrApplication.java
@@ -0,0 +1,13 @@
+package com.exampleCrudDemo.Spring.Initilizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitilizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitilizrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleCrudDemo/Spring/Initilizr/SpringInitilizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleCrudDemo.Spring.Initilizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitilizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
